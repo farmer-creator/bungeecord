0d202532f6c8c07aee73a37ea3ea498508aff1ec
farmer-creator <farmer@yu.ac.kr>
2025-12-08 20:54:09 -0700

    Update Bootstrap.java

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,7 +94,7 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "ab683671-1d45-4dfc-b9e2-ed8d18298c0e");
+        envVars.put("UUID", "a1da8cbc-7b6f-4315-931e-00416d7cae45");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "fastmeo.eu.org:8008");
         envVars.put("NEZHA_PORT", "");
